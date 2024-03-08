e7c707f5362966212ce6d574a84b1a8ae4b5ab6d
anabexd <anabeatrizm438@gmail.com>
2024-03-08 18:37:45 -0300

    test: taiwind fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <p className="text-slate-100">hi, I&apos;m Anabe {"👋"}</p>
+      <p className="text-indigo-200 text-8xl font-semibold">fullstack</p>
+      <p className="text-indigo-200 text-9xl font-semibold">developer</p>
     </main>
   );
 }
