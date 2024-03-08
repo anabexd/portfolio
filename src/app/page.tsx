624fdcb89733dca386f7b1253b0ac89ff8a38c9d
anabexd <anabeatrizm438@gmail.com>
2024-03-08 14:21:45 -0300

    fix: unescaped-entities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-slate-100">hi, I'm Anabe {"👋"}</p>
+      <p className="text-slate-100">hi, I&apos;m Anabe {"👋"}</p>
     </main>
   );
 }
