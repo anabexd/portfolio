23c390fa0950a78c062c158f45e9923eeeea8cca
anabexd <anabeatrizm438@gmail.com>
2024-03-09 22:25:05 -0300

    feat: sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
+import SideBar from "./ui/sideBar";
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center w-100">
+    <>
+    <main className="flex min-h-screen flex-col items-center justify-center w-100 select-none">
+    <SideBar />
       <div className="flex items-left flex-col gap-7">
         <p className="text-slate-100 text-2xl">hi, I&apos;m Anabe {"👋"}</p>
-        <div className="text-indigo-300 leading-loose">
-          <p className="text-6xl font-bold">fullstack</p>
-          <p className="text-9xl font-bold italic">developer</p>
+        <div className="text-indigo-300 leading-loose uppercase">
+          <p className="text-6xl font-extrabold">fullstack</p>
+          <p className="text-8xl font-extrabold">developer</p>
         </div>
         <p className="text-indigo-200 w-3/5 text-xl leading-8 font-semibold opacity-75">
           I&apos;m 18 years old and have been developing systems for 1 year working
@@ -25,5 +30,14 @@ export default function Home() {
         </div>
       </div>
     </main>
+    <main className="flex min-h-screen flex-col items-center justify-top w-100 p-20">
+      <div className="flex items-left flex-col gap-7">
+        <p className="text-slate-100 text-2xl font-mono">{"> "}my projects</p>
+        <div className="text-indigo-300 leading-loose">
+
+      </div>
+      </div>
+    </main>
+    </>
   );
 }
---
+++ src/app/ui/sideBar/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const SideBar = () => {
+  return (
+  <>
+  <div className="flex flex-col fixed left-0 ml-10 border border-indigo-300 divide-y divide-indigo-300 rounded-lg p-3">
+    <Image src="./home.svg" width={30} height={30} alt="" className="pt-2 pb-2 hover:cursor-pointer"></Image>
+    <Image src="./folder.svg" width={30} height={30} alt="" className="opacity-50 pt-2 pb-2 hover:cursor-pointer"></Image>
+    <Image src="./mail.svg" width={30} height={30} alt="" className="opacity-50 pt-2 pb-2 hover:cursor-pointer"></Image>
+  </div>
+  </>
+  )
+};
+
+export default SideBar;
