ecd2f4e8243de4eb2a6355c09c34065786f4ec15
anabexd <anabeatrizm438@gmail.com>
2024-03-11 22:59:41 -0300

    style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,29 +7,39 @@ import SideBar from './ui/sideBar'
 export default function Home (): React.JSX.Element {
   return (
     <>
-    <main className='flex min-h-screen flex-col items-center justify-center w-100 select-none'>
+    <main className='flex min-h-screen flex-row items-center justify-center w-100 select-none' id='main-home'>
     <SideBar />
-      <div className='flex items-left flex-col gap-7'>
-        <p className='text-slate-300 text-2xl font-semibold'>hi, I&apos;m Anabe {'👋'}</p>
-        <div className='text-indigo-300 leading-loose uppercase'>
-          <p className='text-6xl font-extrabold'>fullstack</p>
-          <p className='text-8xl font-extrabold'>developer</p>
+      <div className='flex items-left flex-col gap-12'>
+        <div className="flex flex-row items-center">
+        <p className='text-indigo-100 text-2xl font-semibold'>hi, I&apos;m Anabe</p>
+        <span className='animate-bounce text-2xl ml-2'>{'👋'}</span>
         </div>
-        <p className='text-indigo-200 w-3/5 text-xl leading-8 font-semibold opacity-75'>
+        <div className='font-extrabold italic bg-clip-text text-transparent bg-gradient-to-r from-indigo-100 to-indigo-300'>
+          <p className='text-7xl'>fullstack</p>
+          <p className='text-8xl'>developer</p>
+        </div>
+        <p className='text-indigo-300 w-3/5 text-xl leading-8 font-semibold pt-3'>
           I&apos;m 18 years old and have been developing systems for 1 year working
           as a freelancer.
         </p>
-        <div className='flex gap-5'>
+        <div className='flex gap-6 transition ease-in-out delay-150'>
           <Link target='_blank' href='https://github.com/anabexd'>
-            <Image className='hover:scale-95' src='/mdi_github.svg' width={40} height={40} alt=''></Image>
+            <Image className='hover:-translate-y-1 hover:scale-110 duration-300' src='/mdi_github.svg' width={40} height={40} alt=''></Image>
             </Link>
           <Link target='_blank' href='https://linkedin.com/in/anabe-sc'>
-          <Image className='hover:scale-95' src='/mdi_linkedin.svg' width={40} height={40} alt=''></Image>
+          <Image className='hover:-translate-y-1 hover:scale-110 duration-300' src='/mdi_linkedin.svg' width={40} height={40} alt=''></Image>
           </Link>
           <Link target='_blank' href='https://drive.google.com/file/d/18tVLLld8qB_2HfDr6YQDsj2zGgExbaxE/view?usp=sharing'>
-          <Image className='hover:scale-95' src='/mdi_paper.svg' width={40} height={40} alt=''></Image>
+          <Image className='hover:-translate-y-1 hover:scale-110 duration-300' src='/mdi_paper.svg' width={40} height={40} alt=''></Image>
           </Link>
         </div>
+      </div>
+      <Image src='/profile.png' width={250} height={250} alt='' className='select-none pointer-events-none'></Image>
+    </main>
+    <main className='flex min-h-screen flex-row items-start justify-center w-100'>
+      <div className="divide-y divide-opacity-35 divide-indigo-100 flex flex-col justify-center items-center">
+      <p className='text-indigo-300 text-xl font-semibold'>My projects</p>
+      <p className='text-indigo-300 text-xl font-semibold'>CantinaGO</p>
       </div>
     </main>
     </>
--- src/app/ui/sideBar/index.tsx
+++ src/app/ui/sideBar/index.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 const SideBar = (): React.JSX.Element => {
   return (
   <>
-  <div className="flex flex-col fixed left-0 ml-10 border border-indigo-300 divide-y divide-indigo-300 rounded-lg p-3">
-    <Image src="./home.svg" width={30} height={30} alt="" className="pt-2 pb-2 hover:cursor-pointer"></Image>
-    <Image src="./folder.svg" width={30} height={30} alt="" className="opacity-50 pt-2 pb-2 hover:cursor-pointer"></Image>
-    <Image src="./mail.svg" width={30} height={30} alt="" className="opacity-50 pt-2 pb-2 hover:cursor-pointer"></Image>
+  <div className="flex flex-col fixed left-0 ml-10 border border-indigo-300 divide-y divide-indigo-300 rounded-lg p-4">
+    <Image src="./home.svg" width={25} height={25} alt="" className="pt-3 pb-3 hover:cursor-pointer"></Image>
+    <Image src="./folder.svg" width={25} height={25} alt="" className="opacity-50 pt-3 pb-3 hover:cursor-pointer"></Image>
+    <Image src="./mail.svg" width={25} height={25} alt="" className="opacity-50 pt-3 pb-3 hover:cursor-pointer"></Image>
   </div>
   </>
   )
